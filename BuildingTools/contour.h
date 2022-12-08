f67e88266574f592c910e90ebc4d6805ac6b5f3d
edoardobarba <edoardo.barba1999@gmail.com>
2022-12-08 11:54:00 +0100

    Added Contoour.h file

---
+++ BuildingTools/contour.h
@@ -0,0 +1,14 @@
+#include "point.h"
+
+#ifndef CONTOUR_H
+#define CONTOUR_H
+
+struct Contour{
+    Point vertexOne{};
+    Point vertexTwo{};
+    Point vertexThree{};
+    Point vertexFour{};
+};
+
+
+#endif
