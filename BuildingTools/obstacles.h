9d5ca2a6c1017ef20ba9d3dc3b885bad45dea0f3
edoardobarba <edoardo.barba1999@gmail.com>
2022-12-09 11:15:56 +0100

    test

--- BuildingTools/obstacles.h
+++ BuildingTools/obstacles.h
@@ -1,14 +1,16 @@
 #include "point.h"
 #include "square.h"
 #include "triangle.h"
+#include<list> 
 
 #ifndef OBSTACLES_H
 #define OBSTACLES_H
 
 struct Obstacles{
-        
+
+    list<Triangle> triangles;
+    list<Square> squares;
  
 }
-
 
 #endif
