7dc8c7d5a76c9a99eeda596484096b1bee27506d
Edoardo Barba <104260638+edoardobarba@users.noreply.github.com>
2022-12-21 16:51:12 +0100

    Update path_planning.cpp

--- path_planning/src/path_planning.cpp
+++ path_planning/src/path_planning.cpp
@@ -53,35 +53,36 @@ class MinimalPublisher : public rclcpp::Node
     MinimalPublisher()
     : Node("barba_node")
     {
-      // publisher_ = this->create_publisher<nav_msgs::msg::Path>("plan", 10);
+      publisher_ = this->create_publisher<nav_msgs::msg::Path>("plan", 10);
 
 
-      // //Tranform the frame
-      // std::string target_frame_ = this->declare_parameter<std::string>("target_frame", "gazebo/base_link");
-      // std::shared_ptr<tf2_ros::TransformListener> tf_listener{nullptr};
-      // std::unique_ptr<tf2_ros::Buffer> tf_buffer;
-      // tf_buffer = std::make_unique<tf2_ros::Buffer>(this->get_clock());
-      // tf_listener = std::make_shared<tf2_ros::TransformListener>(*tf_buffer);
-      // std::string fromFrameRel = target_frame_.c_str();
-      // std::string toFrameRel = "map";
-      // geometry_msgs::msg::TransformStamped t;
+      //Tranform the frame
+      std::string target_frame_ = this->declare_parameter<std::string>("target_frame", "gazebo/base_link");
+      std::shared_ptr<tf2_ros::TransformListener> tf_listener{nullptr};
+      std::unique_ptr<tf2_ros::Buffer> tf_buffer;
+      tf_buffer = std::make_unique<tf2_ros::Buffer>(this->get_clock());
+      tf_listener = std::make_shared<tf2_ros::TransformListener>(*tf_buffer);
+      std::string fromFrameRel = target_frame_.c_str();
+      std::string toFrameRel = "map";
+      geometry_msgs::msg::TransformStamped t;
 
-      // try {
-      //   t = tf_buffer->lookupTransform(toFrameRel, fromFrameRel, tf2::TimePointZero, 15s);
-      // } 
-      // catch (const tf2::TransformException & ex) {
-      //   RCLCPP_INFO(this->get_logger(), "Could not transform %s to %s: %s",toFrameRel.c_str(), fromFrameRel.c_str(), ex.what());
-      //   return;
-      // }
+      try {
+        t = tf_buffer->lookupTransform(toFrameRel, fromFrameRel, tf2::TimePointZero, 15s);
+      } 
+      catch (const tf2::TransformException & ex) {
+        RCLCPP_INFO(this->get_logger(), "Could not transform %s to %s: %s",toFrameRel.c_str(), fromFrameRel.c_str(), ex.what());
+        return;
+      }
 
-      // tf2::Quaternion q(t.transform.rotation.x,t.transform.rotation.y,t.transform.rotation.z,t.transform.rotation.w);
-      // tf2::Matrix3x3 m(q);
-      // double roll, pitch, yaw;
-      // m.getRPY(roll, pitch, yaw);
+      tf2::Quaternion q(t.transform.rotation.x,t.transform.rotation.y,t.transform.rotation.z,t.transform.rotation.w);
+      tf2::Matrix3x3 m(q);
+      double roll, pitch, yaw;
+      m.getRPY(roll, pitch, yaw);
 
-      // nav_msgs::msg::Path path;
-      // path.header.stamp = this->get_clock()->now();
-      // path.header.frame_id = "map";
+      nav_msgs::msg::Path path;
+      path.header.stamp = this->get_clock()->now();
+      path.header.frame_id = "map";
+
 
       Environment env = get_environment1();
 
@@ -106,21 +107,8 @@ class MinimalPublisher : public rclcpp::Node
 
       Polyline points_final_path = interpolation(shortest_path, th0, thf, minR);
 
-      for(int i=0; i<points_final_path.size(); i++){
-          double x = points_final_path[i].x();
-          double y = points_final_path[i].y();
-      }
 
 
-
-      /*
-      double x0;
-      double y0;
-      double th0;
-
-      x0 = t.transform.translation.x;
-      y0 = t.transform.translation.y;
-      th0 = t.transform.rotation.z;
 
       std::vector<geometry_msgs::msg::PoseStamped> poses_temp;
 
@@ -129,119 +117,33 @@ class MinimalPublisher : public rclcpp::Node
       geometry_msgs::msg::Quaternion quaternion_temp;
       geometry_msgs::msg::PoseStamped pose_stamped_temp;
 
-      
-      //Define problem data
-      double xf = 0; 
-      double yf = 0; 
-      double thf = -M_PI/3;
-      double Kmax = 3.0;
+      for(int i=0; i<points_final_path.size(); i++){
+          double x = points_final_path[i].x();
+          double y = points_final_path[i].y();
 
+          position_temp.x = x;
+          position_temp.y = y;
+          position_temp.z = 0;
 
+          quaternion_temp.x = 0;
+          quaternion_temp.y = 0;
+          quaternion_temp.z = 0;
+          quaternion_temp.w = 0;
 
-      //Find optimal Dubins solution
-      Curve sol;
-      c = dubins_shortest_path(x0, y0, th0, xf, yf, thf, Kmax);
-      string types[6] = {"LSL", "RSR", "LSR", "RSL", "RLR", "LRL"};
+          pose_temp.position = position_temp;
+          pose_temp.orientation = quaternion_temp;
 
-      Arc a1 = c.a1;
-      Arc a2 = c.a2;
-      Arc a3 = c.a3;
-      double L = c.L;
+          pose_stamped_temp.pose = pose_temp;
+          pose_stamped_temp.header.stamp = this->get_clock()->now();
+          pose_stamped_temp.header.frame_id = "map";
 
-      int npts = 100;
-    
-      for (int j=0; j<npts; j++){
-
-
-        double s = a1.L/npts * j;
-
-        Pos point;
-        point = circline(s, a1.x0, a1.y0, a1.th0, a1.k);
-
-        position_temp.x = point.x;
-        position_temp.y = point.y;
-        //position_temp.x = j;
-        //position_temp.y = j;
-        position_temp.z = 0;
-
-        quaternion_temp.x = 0;
-        quaternion_temp.y = 0;
-        quaternion_temp.z = 0;
-        quaternion_temp.w = 0;
-
-        pose_temp.position = position_temp;
-        pose_temp.orientation = quaternion_temp;
-
-        pose_stamped_temp.pose = pose_temp;
-        pose_stamped_temp.header.stamp = this->get_clock()->now();
-        pose_stamped_temp.header.frame_id = "map";
-
-        poses_temp.push_back(pose_stamped_temp);
+          poses_temp.push_back(pose_stamped_temp);
       }
-
-      for (int j=0; j<npts; j++){
-
-        
-        double s = a2.L/npts * j;
-
-        Pos point;
-        point = circline(s, a2.x0, a2.y0, a2.th0, a2.k);
-
-        position_temp.x = point.x;
-        position_temp.y = point.y;
-        //position_temp.x = j;
-        //position_temp.y = j;
-        position_temp.z = 0;
-
-        quaternion_temp.x = 0;
-        quaternion_temp.y = 0;
-        quaternion_temp.z = 0;
-        quaternion_temp.w = 0;
-
-        pose_temp.position = position_temp;
-        pose_temp.orientation = quaternion_temp;
-
-        pose_stamped_temp.pose = pose_temp;
-        pose_stamped_temp.header.stamp = this->get_clock()->now();
-        pose_stamped_temp.header.frame_id = "map";
-
-        poses_temp.push_back(pose_stamped_temp);
-      }
-
-      for (int j=0; j<npts; j++){
-
-        
-        double s = a3.L/npts * j;
-
-        Pos point;
-        point = circline(s, a3.x0, a3.y0, a3.th0, a3.k);
-
-        position_temp.x = point.x;
-        position_temp.y = point.y;
-        //position_temp.x = j;
-        //position_temp.y = j;
-        position_temp.z = 0;
-
-        quaternion_temp.x = 0;
-        quaternion_temp.y = 0;
-        quaternion_temp.z = 0;
-        quaternion_temp.w = 0;
-
-        pose_temp.position = position_temp;
-        pose_temp.orientation = quaternion_temp;
-
-        pose_stamped_temp.pose = pose_temp;
-        pose_stamped_temp.header.stamp = this->get_clock()->now();
-        pose_stamped_temp.header.frame_id = "map";
-
-        poses_temp.push_back(pose_stamped_temp);
-      }
-
 
       path.poses = poses_temp;
       
 
-      */
+
       /*
       //ACTIVATE THE MOTORS
       rclcpp::Client<std_srvs::srv::SetBool>::SharedPtr client_;
@@ -285,7 +187,7 @@ class MinimalPublisher : public rclcpp::Node
       // goal_msg.controller_id = "FollowPath";
       // RCLCPP_INFO(this->get_logger(), "Sending goal");
       // client_ptr->async_send_goal(goal_msg);
-      // publisher_->publish(path);
+      publisher_->publish(path);
       
     }
   
