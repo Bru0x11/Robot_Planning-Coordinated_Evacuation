442e46e421f64362d312ddcc54be74c2f190353e
Edoardo Barba <104260638+edoardobarba@users.noreply.github.com>
2022-12-21 15:59:27 +0100

    Update interpolation.h

--- Interpolation/include/interpolation.h
+++ Interpolation/include/interpolation.h
@@ -48,6 +48,8 @@ class Line{
     static double get_q_from_point(Point p, double m);
         
     static double compute_m(Point p0, Point p1);
+    static Line::get_line_from_m_and_p(Point p, double m);
+
         
 };
 
