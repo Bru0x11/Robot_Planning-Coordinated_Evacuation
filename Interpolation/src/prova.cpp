9c237a8b31f01be55f18607d02f7188477c79d65
Edoardo Barba <104260638+edoardobarba@users.noreply.github.com>
2022-12-26 16:57:12 +0100

    solved interpolation problems (maybe))

--- Interpolation/src/prova.cpp
+++ Interpolation/src/prova.cpp
@@ -9,27 +9,28 @@ using namespace std;
 int main(){
     string filename = "example1.environment";
 
-    Line l = Line(1,1,0);
-    cout<<"m: "<<l.m<<endl;
+    //Line l = Line(1,1,0);
+    //cout<<"m: "<<l.m<<endl;
     //cout<<"angle: "<<compute_angle(Point(0,0), Point(-1,5))<<endl;
     //cout<<"angle: "<<compute_angle(Point(0,0), Point(-5,5))<<endl;
 
-    sleep(5);
+    //sleep(5);
 
     Environment env = Environment(filename);
-    cout<<"ho letto env"<<endl;
+    //cout<<"ho letto env"<<endl;
 
     //ROAD MAP
     Visibility_Graph graph = Visibility_Graph(env, 0.1);
 
     //DEFINE ROBOT MIN_CURVATURE_RADIUS
     double minR = 1.5;
-    //DEFINE START AND END POINTS
-    double x0 = 0;
-    double y0 = 0;
 
-    Point start_test = Point(0, 0);
-    Point end = Point(4.0, 17.0);
+    //DEFINE START AND END POINTS
+    //double x0 = 0;
+    //double y0 = 0;
+
+    Point start_test = Point(5, 2);
+    Point end = Point(12.0, 14.0);
     //DEFINE START AND END ANGLES 
     double th0 = 0;
     double thf = M_PI/2;
@@ -45,10 +46,11 @@ int main(){
     cout<<env<<endl;
 
     //cout << "graph: "<<endl<<graph<<endl;
-    sleep(5); 
+    //sleep(5); 
 
-    cout<<"faccio interpolation"<<endl;
+    cout<<"do interpolation"<<endl;
 
     Polyline points_final_path = interpolation(shortest_path, th0, thf, minR);
+
     return 0;
 }
