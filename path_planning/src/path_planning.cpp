111264a55cd100b1004ba6f6cd0a31a5275decfd
Edoardo Barba <104260638+edoardobarba@users.noreply.github.com>
2022-12-27 15:36:41 +0100

    Update path_planning.cpp

--- path_planning/src/path_planning.cpp
+++ path_planning/src/path_planning.cpp
@@ -182,18 +182,18 @@ class MinimalPublisher : public rclcpp::Node
       */
 
       //ACTION 
-      using FollowPath = nav2_msgs::action::FollowPath;
-      rclcpp_action::Client<FollowPath>::SharedPtr client_ptr;
-      client_ptr = rclcpp_action::create_client<FollowPath>(this,"follow_path");
-      if (!client_ptr->wait_for_action_server()) {
-        RCLCPP_ERROR(this->get_logger(), "Action server not available after waiting");
-        rclcpp::shutdown();
-      }
-      auto goal_msg = FollowPath::Goal();
-      goal_msg.path = path;
-      goal_msg.controller_id = "FollowPath";
-      RCLCPP_INFO(this->get_logger(), "Sending goal");
-      client_ptr->async_send_goal(goal_msg);
+      // using FollowPath = nav2_msgs::action::FollowPath;
+      // rclcpp_action::Client<FollowPath>::SharedPtr client_ptr;
+      // client_ptr = rclcpp_action::create_client<FollowPath>(this,"follow_path");
+      // if (!client_ptr->wait_for_action_server()) {
+      //   RCLCPP_ERROR(this->get_logger(), "Action server not available after waiting");
+      //   rclcpp::shutdown();
+      // }
+      // auto goal_msg = FollowPath::Goal();
+      // goal_msg.path = path;
+      // goal_msg.controller_id = "FollowPath";
+      // RCLCPP_INFO(this->get_logger(), "Sending goal");
+      // client_ptr->async_send_goal(goal_msg);
       publisher_->publish(path);
       
     }
