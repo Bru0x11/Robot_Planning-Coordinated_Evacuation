34146831226dae7fd47d32f6a184a3329275a665
Edoardo Barba <104260638+edoardobarba@users.noreply.github.com>
2022-12-27 18:07:51 +0100

    Update path_planning.cpp

--- path_planning/src/path_planning.cpp
+++ path_planning/src/path_planning.cpp
@@ -56,7 +56,7 @@ class MinimalPublisher : public rclcpp::Node
       publisher_ = this->create_publisher<nav_msgs::msg::Path>("plan", 10);
 
       //Tranform the frame
-      std::string target_frame_ = this->declare_parameter<std::string>("target_frame", "gazebo/base_link");
+      std::string target_frame_ = this->declare_parameter<std::string>("target_frame", "shelfino1/base_link");
       std::shared_ptr<tf2_ros::TransformListener> tf_listener{nullptr};
       std::unique_ptr<tf2_ros::Buffer> tf_buffer;
       tf_buffer = std::make_unique<tf2_ros::Buffer>(this->get_clock());
@@ -80,7 +80,7 @@ class MinimalPublisher : public rclcpp::Node
 
       nav_msgs::msg::Path path;
       path.header.stamp = this->get_clock()->now();
-      path.header.frame_id = "shelfino1/map";
+      path.header.frame_id = "map";
 
 
       Environment env = get_environment1();
