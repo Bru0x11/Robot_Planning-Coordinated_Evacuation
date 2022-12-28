4d0b00735c129753b9f3b5d44e3aad4891a09d0a
Edoardo Barba <104260638+edoardobarba@users.noreply.github.com>
2022-12-28 10:08:12 +0100

    Update path_planning.cpp

--- path_planning/src/path_planning.cpp
+++ path_planning/src/path_planning.cpp
@@ -56,7 +56,7 @@ class MinimalPublisher : public rclcpp::Node
       publisher_ = this->create_publisher<nav_msgs::msg::Path>("plan", 10);
 
       //Tranform the frame
-      std::string target_frame_ = this->declare_parameter<std::string>("target_frame", "shelfino1/base_link");
+      std::string target_frame_ = this->declare_parameter<std::string>("target_frame", "shelfino1");
       std::shared_ptr<tf2_ros::TransformListener> tf_listener{nullptr};
       std::unique_ptr<tf2_ros::Buffer> tf_buffer;
       tf_buffer = std::make_unique<tf2_ros::Buffer>(this->get_clock());
