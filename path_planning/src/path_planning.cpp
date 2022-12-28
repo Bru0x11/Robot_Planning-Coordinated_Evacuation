f0e0a1c12cc7fe396ec28b7062e70348be20116e
Edoardo Barba <104260638+edoardobarba@users.noreply.github.com>
2022-12-28 10:16:35 +0100

    Update path_planning.cpp

--- path_planning/src/path_planning.cpp
+++ path_planning/src/path_planning.cpp
@@ -65,7 +65,7 @@ class MinimalPublisher : public rclcpp::Node
       tf_buffer = std::make_unique<tf2_ros::Buffer>(this->get_clock());
       tf_listener = std::make_shared<tf2_ros::TransformListener>(*tf_buffer);
       std::string fromFrameRel = target_frame_.c_str();
-      std::string toFrameRel = "map"; //map
+      std::string toFrameRel = "shelfino1/map"; //map
       geometry_msgs::msg::TransformStamped t;
 
       try {
