06837ae375f77474e1d2689be13b3401120b3cde
Bru0x11 <matteobrugnera737@gmail.com>
2023-02-13 11:33:16 +0100

    Update offsetFunctions.cpp

--- OffsetTools/src/offsetFunctions.cpp
+++ OffsetTools/src/offsetFunctions.cpp
@@ -56,9 +56,7 @@ void checkIntersectionsRec(const PathsD& newPolygon, std::vector<PathsD>& previo
       checkIntersectionsRec(newPolygon, previousPolygons, i+1);
     }
   }else{
-    if(merged_poly != NULL) {
-      previousPolygons.push_back(merged_poly);
-    }
+      previousPolygons.push_back(newPolygon);
   }
 }
 
